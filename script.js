bb90bbece7d7b54589559a53f52b0e4a149aea2c
Daniel <clancy.daniel@gmail.com>
2018-02-08 10:22:36 -0600

    added some comments

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 $(document).ready(function() {
 
+	// hides a ninja
 	$('img').click(function() {
 		$(this).fadeOut(500);
 	});
 
+	// shows all the ninjas
 	$('.button').click(function() {
 		$('img').show();
 	});
